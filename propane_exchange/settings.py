ba2166fb3290c0bd6010f1114d18ff81c038759c
unknown <lienujacnalballiv@gmail.com>
2025-11-24 20:58:21 +0800

    Fix CSRF error for Render deployment

--- propane_exchange/settings.py
+++ propane_exchange/settings.py
@@ -33,7 +33,12 @@ ALLOWED_HOSTS = ['prophane.onrender.com', 'localhost', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = [
     'https://prophane.onrender.com',
 ]
-
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
 # Application definition
 
 INSTALLED_APPS = [
@@ -233,9 +238,3 @@ LOGGING = {
 }
 
 # Security settings for production
-if not DEBUG:
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SECURE_BROWSER_XSS_FILTER = True
-    SECURE_CONTENT_TYPE_NOSNIFF = True
